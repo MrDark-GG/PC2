00b2938f3202dd25959a39681be4bf5f26fe5bfa
MrDark-GG <Mr.Dark_GG@protonmail.ch>
2021-04-27 19:28:26 -0500

    Creacion de CONTACTENOS

---
+++ Models/Contactenos.cs
@@ -0,0 +1,10 @@
+namespace PC2.Models
+{
+    public class Contactenos
+    {
+        public string Nombre {get; set;}
+        public string Apellido {get; set;}
+        public string Correo {get; set;}
+        public string Mensaje {get; set;}
+    }
+}
