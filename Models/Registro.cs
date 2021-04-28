4537ba307044b1041bfce717562adbae07346ac4
MrDark-GG <Mr.Dark_GG@protonmail.ch>
2021-04-27 19:28:46 -0500

    CREACION DE REGISTRO

---
+++ Models/Registro.cs
@@ -0,0 +1,12 @@
+namespace PC2.Models
+{
+    public class Registro
+    {
+        
+        public string Nombre {get; set;}
+        public string Apellidos{get; set;}
+        public string Usuario {get; set;}
+        public string Contraseña{get; set;}
+        public string Email{get; set;}
+    }
+}
