d72a6fbabb58f380934d292de5cd4fccd51de823
MrDark-GG <Mr.Dark_GG@protonmail.ch>
2021-04-27 19:26:52 -0500

    Creacion de contacto

---
+++ Controllers/ContactenosControllers.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PC2.Controllers
+{
+    public class ContactenosControllers : Controller
+    {
+        public IActionResult contactenos(){
+            return View();
+        }
+    }
+}
